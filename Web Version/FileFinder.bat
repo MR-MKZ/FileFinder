059f065669e64a12432119cc0beaa94a7f9daf34
Mr.MKZ <74706544+MR-MKZ@users.noreply.github.com>
2023-08-21 11:42:40 +0330

    The execution problem has been resolved
    Problem running script on windows without python was fixed

---
+++ Web Version/FileFinder.bat
@@ -0,0 +1,7 @@
+@echo off
+title Install/Update requirements
+python3.11\python.exe -m pip install -r requirements.txt --no-warn-script-location
+
+title File Finder - Created By Mr.MKZ
+cls
+python3.11\python.exe main.py
